f9ae60d3aa889827343acfde672738cd52205835
Marcus Janum <dsx992@ku.dk>
2026-02-24 22:31:49 +0100

    simple tal-0 interpreter from advanced topics in types and programming languages exercises

--- extra/tal-0-interpreter.fsx
+++ extra/tal-0-interpreter.fsx
@@ -1,62 +1,103 @@
+module TAL0
 
-
-type register = R1 | R2 | R3 | R4 | R5 | R6
-and value =
-    | I of int
-    | A of address
+type register = R0 | R1 | R2 | R3 | R4
 and address =
     | Label of string
     | Pointer of int
-and operand = 
-    | V of value
+and operand =
+    | N of int
+    | L of string
     | R of register
 and instruction =
     | Mov of register * operand
-    | Add of register * operand * operand
-    | If of register * operand
+    | Add of register * register * operand
+    | Ifj of register * operand
+    | Print of register
 and sequence =
     | Jump of operand
     | Cons of instruction * sequence
-    // | list?
 
-let regadd (regs : register -> value option) ((r, v) : register * value): register -> value option =
-    fun r' -> if r' = r then Some v else regs r'
-    
+let regadd (rlu : register -> operand) ((r, v) : register * operand) =
+    fun r' -> if r = r' then v else rlu r'
 
-let rec eval (s : sequence) (registers : register -> value option) (labels : address -> sequence) =
+let rec eval s rlu labels =
     match s with
     | Jump op ->
         match op with
-        | R r ->
-            match registers r with
-            | Some v -> eval (Jump (V v)) registers labels
-            | _ -> eprintf $"register {r} has is unbound"
-        | Cons (insn, s') -> evali insn
+        | R R0 -> rlu
+        | N n -> eval (labels (Pointer n)) rlu labels
+        | L l -> eval (labels (Label l)) rlu labels
+        | R r -> let op' = rlu r in eval (Jump op') rlu labels
+    | Cons (insn, s') ->
+        match evali insn rlu labels with
+        | rlu', None -> eval s' rlu' labels
+        | rlu', Some s'' -> eval s'' rlu' labels
 
-and evali (insn : instruction) (registers : register -> value option) (labels : address -> sequence) =
+and evali insn rlu labels : (register -> operand) * sequence option =
     match insn with
-    | Mov (r, o) ->
-        match o with
-        | V v -> regadd registers (r, v)
-        | R r' ->
-            match registers r' with
-            | Some v -> regadd registers (r', v)
-            | None -> registers
-    | Add (r, o, p) ->
-        
-let program : sequence list * (address -> sequence) =
-    let code = [
-        "prod", Cons( Mov(R3, N 0), 
-                      Jump (L (Label "loop")))
-        "loop", Cons( If (R1, (L (Label "done"))),
-                Cons( Add(R3, R R2, R R3),
-                Cons( Add(R1, R R1, N -1),
-                      Jump (L (Label "loop")))))
-        "done",       Jump (R R4)
-    ]
-    let maddr a =
-        match a with
-        | Pointer _ -> failwith "No pointers"
-        | Label l -> List.find (fst >> (=) l) code |> snd
+    | Mov (rd, op) -> regadd rlu (rd, op), None
+    | Add (rd, rs, v) ->
+        let rec add = function
+            | N n, N m -> regadd rlu (rd, N (n + m))
+            | R r, v' -> add (rlu r, v')
+            | v', R r -> add (v', rlu r)
+            | L _, _
+            | _, L _ -> failwith "cannot add labels"
+        let r = rlu rs in add (r, v), None
+    | Ifj (r, v) ->
+        let rec bound = function
+            | N n -> n = 0
+            | L _ -> false
+            | R r -> bound (rlu r)
+        let rec code = function
+            | N n -> labels (Pointer n)
+            | L l -> labels (Label l)
+            | R r -> code (rlu r)
+        rlu, if bound (rlu r) then Some (code v) else None
+    | Print r ->
+        printfn "print: %A" (rlu r)
+        rlu, None
 
-    (List.map snd code), maddr
+
+let heap : (address -> sequence) =
+    let code = [
+        Label "prod",
+          Cons(   Mov(R3, N 0), 
+                  Jump (L  "loop"))
+        Label "loop",
+          Cons(   Ifj (R1, (L "done")),
+          Cons(   Add(R3, R2, R R3),
+          Cons(   Add(R1, R1, N -1),
+                  Jump (L "loop"))))
+        Label "done",
+                    Jump (R R4)
+        Label "print",
+            Cons (  Print R3, Jump (R R0))
+    ]
+    fun l -> List.find (fst >> (=) l) code |> snd
+
+
+let prod n m =
+    let rlu r =
+        match r with
+        | R1 -> N n
+        | R2 -> N m
+        | R4 ->  L "print"
+        | _ -> failwith $"register {r} not bound."
+
+    (eval (heap (Label "prod")) rlu heap) R3
+
+
+[<EntryPoint>]
+let main _ =
+    let n, m = 3, 4
+    let rlu r =
+        match r with
+        | R1 -> N n
+        | R2 -> N m
+        | R4 ->  L "print"
+        | _ -> failwith $"register {r} not bound."
+
+    eval (heap (Label "prod")) rlu heap |> ignore
+    0
+
