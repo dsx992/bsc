5ae098166ed3cbb31f965945d355c81239306a60
Marcus Janum <dsx992@ku.dk>
2026-05-25 21:17:24 +0200

    actually started implementation wee

---
+++ src/Syntax.hs
@@ -0,0 +1,70 @@
+module Syntax where
+class Var a where
+    name :: a -> String
+    newvar :: String -> a
+
+newtype ATypeVar = ATypeVar String
+    deriving (Show)
+instance Var ATypeVar where
+    name (ATypeVar str) = str
+    newvar = ATypeVar
+
+newtype MemTypeVar = MemTypeVar String
+    deriving (Show)
+instance Var MemTypeVar where
+    name (MemTypeVar str) = str
+    newvar = MemTypeVar
+
+newtype StackTypeVar = StackTypeVar String
+    deriving (Show)
+instance Var StackTypeVar where
+    name (StackTypeVar str) = str
+    newvar = StackTypeVar
+
+data TypeVar = Alpha ATypeVar
+             | Mem MemTypeVar
+             | Stack StackTypeVar
+    deriving (Show)
+
+data Register = Zero | Ra | Sp | Gp | Tp | T0 | T1 | T2 | Fp | S1 | A0 | A1 | A2 | A3 | A4 | A5 | A6 | A7 | S2 | S3 | S4 | S5 | S6 | S7 | S8 | S9 | S10 | S11 | T3 | T4 | T5 | T6
+    deriving (Eq, Show, Enum)
+
+-- newtype StateType = StateType TypeVarContext RegFile
+
+type TypeVarContext = [(TypeVar, Type)]
+
+type RegFile = (Register -> Type)
+
+data Type = TVar TypeVar | Top | TInt | TDInt Int | TCollection CollectionType
+    deriving (Show)
+
+data StackType = SEmpty | SVar StackTypeVar | Type `SCons` StackType
+    deriving (Show)
+
+data MemType = Scratch | Shared | MVar MemTypeVar
+    deriving (Show)
+
+data CollectionType = Array MemType Type Int | CStack MemType StackType
+    deriving (Show)
+type Label = String
+data Value = VInt Int | Label Label | Reg Register
+    deriving (Show)
+
+-- data ArithOp = Add | Sub | Mul | Div
+data Instruction = Aop Register Register Value
+                 | Incr Register Register Value
+                 | Decr Register Register Value
+                 | Mv Register Register
+                 -- | Jump Value
+                 -- | Halt
+                 | Store Register Register Value
+                 | Load Register Register Value
+                 | Mvscr Register
+                 | Mvshr Register
+    deriving (Show)
+data InstructionSeq = Halt | Jump Value |  Instruction `ICons` InstructionSeq
+    deriving (Show)
+newtype StateType = StateType (TypeVarContext, RegFile)
+newtype Block = Block (RegFile, InstructionSeq)
+newtype LabelMap = LabelMap (Label -> StateType)
+type Program = [(Label, Block)]
